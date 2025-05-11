d80dba6bfdd506d914255a45b752151511a4d617
johnz612 <151622271+johnz612@users.noreply.github.com>
2025-05-11 11:28:16 +0800

    make hero page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import hero from "../assets/hero.png";
 
 import Button from "../ui/Button";
 
@@ -10,11 +11,26 @@ function Home() {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen select-none">
-      <div>
-        <h1 className="font-bold text-black">Free CV Maker</h1>
-        <p>Start Building your CV for free now!</p>
-        <Button onClick={handleClick}>Start Building</Button>
+    <div
+      className={
+        "relative h-screen bg-center bg-cover bg-[url(../assets/hero.png)]"
+      }
+    >
+      {/* dark overlay */}
+      <div className="absolute inset-0 bg-black/25" />
+
+      {/* content */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-center">
+        <h1 className="text-5xl font-bold text-white">Free CV Maker</h1>
+        <p className="mt-4 text-lg text-gray-200">
+          Start building your CV for free now!
+        </p>
+        <button
+          onClick={handleClick}
+          className="px-3 py-3 hover:scale-105 mt-6 font-black cursor-pointer rounded bg-[#163853]"
+        >
+          <span className="text-lg font-bold text-white ">Start Building</span>
+        </button>
       </div>
     </div>
   );
